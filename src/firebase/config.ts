38222c2f55a752123efc0acb136f411370968ced
Ridwan Sihara <sihararidwansihara@gmail.com>
2025-12-18 04:11:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4338156433-fb69a",
+  "appId": "1:498119971710:web:2f6a5ec8c767fcb943192f",
+  "apiKey": "AIzaSyA6wriBNseD1OarfxYwjxe_IueDO33E_2w",
+  "authDomain": "studio-4338156433-fb69a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "498119971710"
+};
